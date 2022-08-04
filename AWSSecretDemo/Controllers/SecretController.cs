67171480275845b698d021e4494cb10eeca0670a
Supreet <suprith.prakash.sp@gmail.com>
2022-08-04 12:41:40 +0530

    Add project files.

---
+++ AWSSecretDemo/Controllers/SecretController.cs
@@ -0,0 +1,118 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AWSSecretDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretController : ControllerBase
+    {
+        // GET: api/<SecretController>
+        [HttpGet]
+        public string Get()
+        {
+
+            string secretName = "lastname";
+            string region = "us-west-2";
+            string secret = "";
+            string secretKey = "AKIA4EC6ZPHX2OUZ3PSN";
+            string pwd = "wDtpGzaL63ugNc4wzFz+O0IJ5Hneq2vqJbPesTU6";
+            MemoryStream memoryStream = new MemoryStream();
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(secretKey,pwd,RegionEndpoint.GetBySystemName(region));
+            GetSecretValueRequest request = new GetSecretValueRequest();
+            request.SecretId = secretName;
+            request.VersionStage = "AWSCURRENT"; // VersionStage defaults to AWSCURRENT if unspecified.
+
+            GetSecretValueResponse response = null;
+
+            // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+            // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            // We rethrow the exception by default.
+
+            try
+            {
+                response = client.GetSecretValueAsync(request).Result;
+            }
+            catch (DecryptionFailureException e)
+            {
+                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (InternalServiceErrorException e)
+            {
+                // An error occurred on the server side.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (InvalidParameterException e)
+            {
+                // You provided an invalid value for a parameter.
+                // Deal with the exception here, and/or rethrow at your discretion
+                throw;
+            }
+            catch (InvalidRequestException e)
+            {
+                // You provided a parameter value that is not valid for the current state of the resource.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (ResourceNotFoundException e)
+            {
+                // We can't find the resource that you asked for.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+            catch (System.AggregateException ae)
+            {
+                // More than one of the above exceptions were triggered.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw;
+            }
+
+            // Decrypts secret using the associated KMS key.
+            // Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if (response.SecretString != null)
+            {
+                secret = response.SecretString;
+            }
+            else
+            {
+                memoryStream = response.SecretBinary;
+                StreamReader reader = new StreamReader(memoryStream);
+                string decodedBinarySecret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+            }
+            return secret;
+
+        }
+
+        // GET api/<SecretController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SecretController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SecretController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SecretController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
